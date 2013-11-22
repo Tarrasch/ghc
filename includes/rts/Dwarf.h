90bf268dd016f2f0443ed0e4f66f206d5048ca40
Arash Rouhani <rarash@student.chalmers.se>
2013-11-22 20:40:20 +0100

    Allow the primitives for GHC.ExecutionStack
    There's still work left. In particular the Dwarf functions should call
    dwarf_init() by themselves. This is to simplify the Haskell API later
    on. Also dwarf_init() have to change, so that calling it twice has no
    effect.
    
    NOTE: 5 Feb 2014: I "fixuped" the delete commits later, and now
    this should be an empty commit, but isnt due to the shit that
    I committed aswell when I first commited
    
    Conflicts:
    	includes/rts/Dwarf.h
    
    Change dwarf_lookup_and_write
    
    Hoping to making it suitable for a primop
    
    Fix signature

--- includes/rts/Dwarf.h
+++ includes/rts/Dwarf.h
@@ -64,7 +64,7 @@ struct DebugInfo_ {
 
 StgWord dwarf_get_debug_info(DwarfUnit *unit, DwarfProc *proc, DebugInfo *infos, StgWord max_infos);
 
-StgWord dwarf_lookup_ip(void *ip, DwarfUnit** p_unit, DebugInfo *infos, int max_num_infos);
+StgWord dwarf_lookup_ip(void *ip, DwarfUnit **p_unit, DebugInfo *infos, int max_num_infos);
 
 StgWord dwarf_addr_num_infos(void *ip);
 
--- rts/Dwarf.c
+++ rts/Dwarf.c
@@ -1155,4 +1155,14 @@ StgWord dwarf_get_debug_info(DwarfUnit *unit, DwarfProc *proc, DebugInfo *infos,
 	return info;
 }
 
+StgWord dwarf_lookup_ip(void *ip, DwarfUnit **p_unit, DebugInfo *infos, int max_num_infos)
+{
+    DwarfProc *proc = dwarf_lookup_proc(ip, p_unit);
+    if (proc == NULL) {
+      return 0;
+    }
+    StgWord infoCount = dwarf_get_debug_info(*p_unit, proc, infos, max_num_infos);
+    return infoCount;
+}
+
 #endif /* USE_DWARF */
