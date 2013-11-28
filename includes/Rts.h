e518bb216742317693f0e39a8409263aac059f56
Arash Rouhani <rarash@student.chalmers.se>
2013-11-28 05:53:42 +0100

    Arash's changes to Dwarf.c and Dwarf.h
    I've squashed many commits here. Here's the summary:
    
      * Make Dwarf.h public (so we can call in from Haskell land)
      * Add some C-functions that are directly adjusted to be sensible
        to call from Haskell land.
      * Ensure we initialize dwarf on calls, just to make the API more
        convenient
      * Rename methods to be easier to understand. For example opposite of
        "load" is now called simply "unload" rather than "free".
      * Add locks so this can be used in a multithreaded setting.
      * Initialize the module in RtsStartup.c (is needed now to initialize
        the locks)
      * Factor out some helper methods

--- includes/Rts.h
+++ includes/Rts.h
@@ -239,6 +239,7 @@ INLINE_HEADER Time fsecondsToTime (double t)
 #include "rts/PrimFloat.h"
 #include "rts/Main.h"
 #include "rts/StaticPtrTable.h"
+#include "rts/Dwarf.h"
 
 /* Misc stuff without a home */
 DLL_IMPORT_RTS extern char **prog_argv; /* so we can get at these from Haskell */
