231d3cd46609d547ba525240372962b444a20a16
Arash Rouhani <rarash@student.chalmers.se>
2014-01-02 17:08:25 +0100

    Add file rts/Stack.{c,h}
    I felt this was necessary, as there's no real reason to cram the stack
    related functions into rts/Schedule.c

---
+++ rts/Stack.h
@@ -0,0 +1,19 @@
+/* -----------------------------------------------------------------------------
+ *
+ * (c) The GHC Team 2014-2014
+ *
+ * Prototypes for functions in Stack.c 
+ * (Functions operating on the STG stack)
+ *
+ * -------------------------------------------------------------------------*/
+
+#ifndef STACK_H
+#define STACK_H
+
+#include "Capability.h"
+
+#include "BeginPrivate.h"
+
+#include "EndPrivate.h"
+
+#endif /* STACK_H */
