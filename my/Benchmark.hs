c0cf289a3b788963ab6227258ec2813b38ded806
Arash Rouhani <rarash@student.chalmers.se>
2014-03-26 15:02:15 +0100

    Benchmark.hs

--- my/Benchmark.hs
+++ my/Benchmark.hs
@@ -1,38 +1,37 @@
-module Main where
-
 import Control.Exception (catch, SomeException (SomeException), evaluate)
 import System.IO.Unsafe (unsafePerformIO)
 import Data.List (foldl')
+import System.CPUTime (getCPUTime)
 
 main :: IO()
 main = do
-    putStrLn $ "Takes " ++ show fast ++ " seconds with small stack"
-    putStrLn $ "Takes " ++ show slow ++ " seconds with large stack"
+    evaluate (stackBuilder 1) -- For fairness with CAFs
+    putStrLn $ "Takes " ++ show fast ++ "ms with small stack"
+    putStrLn $ "Takes " ++ show slow ++ "ms with large stack"
   where
-    fast = (stackBuilder 100)
-    slow = (stackBuilder 10000)
+    fast = stackBuilder 0
+    slow = stackBuilder 1000
 
--- We start here, first we try to put a lot of stuff on the execution stack
-step1 :: Int
-step1 = stackBuilder 100000
+getMilliSeconds :: IO Integer
+getMilliSeconds = fmap (`div` 1000000000) getCPUTime
 
--- This function is extremely unpure in reality, it will return the time it took to evaluate
--- step2
-stackBuilder :: Int -> Double
-stackBuilder 0 = timeIt (step2 )
-stackBuilder x = x + (stackBuilder (x-1)) -- Add frames on the stack
+timeIt :: Integer -> IO Integer
+timeIt x = do t0 <- getMilliSeconds
+              evaluate x
+              t <- getMilliSeconds
+              return (t - t0)
 
-fastSum :: [Int] -> Int
-fastSum = foldl' (+) 0
+-- Returns the time it takes to evaluate stackDependent
+stackBuilder :: Integer -> Integer
+stackBuilder x | x < 1 = unsafePerformIO (timeIt (stackDependent x))
+stackBuilder x         = x - x + stackBuilder (x-1) -- Add frames on the stack
 
--- Next we are here with a big stack behind us probably, we will not perform a
--- pure calculation that relies on exception handling for control flow
-step2 :: Int -> Int
-step2 upto = fastSum (map stupidFunction [1..upto])
+-- It has to take an argument to not become a thunk
+stackDependent :: Integer -> Integer
+stackDependent zero = foldl' (+) 0 (map stupidFunction [1..(zero + 1000000)])
 
-stupidFunction :: Int -> Int
-stupidFunction x | odd x     = x
-                 | otherwise = unsafePerformIO (action `catch` handler)
+stupidFunction :: Integer -> Integer
+stupidFunction x = unsafePerformIO (action `catch` handler)
   where
-    action  = evaluate ((5 :: Int) `div` 0)
-    handler (SomeException _) = return (-x)
+    action = evaluate (5 `div` 0)
+    handler (SomeException _) = return x
