7aafbb6e8cd6da22c128746819c1fd2c3f1e7f46
Arash Rouhani <arash@spotify.com>
2014-08-31 21:55:17 +0200

    Add module ExecutionStack.hsc to base
    The module is an Haskell interface to displaying Stack traces based on
    the execution stack. The interface is meant to be simple enough to be
    exposed directly to any intermediate Haskell user.
    
    This new module relies on:
    
         * Peter Wortmann's DWARF generation patches (-g flag)
         * The section `.debug_ghc` section, which was *not* introduced in
           https://phabricator.haskell.org/D396
         * Dwarf.c and the structure it assumes on the debug_ghc section.
         * Some exposed rts functions in Dwarf.h.
    
    Note that this patch does not contain the changes for retrieving the
    stack trace port-mortem.  That is a different beast and more
    controversial, as it would be more changes rather than only additions.
    
    Conflicts:
    	libraries/base/tests/all.T
    
    Arash: Amend test 004 and Add test 005
    
    Comments 005.hs

---
+++ libraries/base/tests/executionStack005.hs
@@ -0,0 +1,42 @@
+import GHC.ExecutionStack
+import Data.List (isInfixOf)
+import Control.Concurrent.MVar (MVar, newEmptyMVar, takeMVar, putMVar)
+import Control.Concurrent (forkIO)
+
+-- In this test we check that it is thread-safe, which means that a thread
+-- can't unload the dwarf data while another thread is in an `inDwarf` block.
+
+-- NOTE TO ARASH: Not tested to compile/run yet!
+
+dwarfIsLoaded :: IO Bool
+dwarfIsLoaded = fmap (("Data not found" `isInfixOf`) . show) currentExecutionStack
+
+main :: IO ()
+main = do
+    var1 <- newEmptyMVar
+    var2 <- newEmptyMVar
+    dwarfIsLoaded >>= (check . not)       -- 1
+    dwarfTryUnload >>= (check . not)
+    thread <- forkIO $ do
+        takeMVar var1
+        -- When doing dwarfIsLoaded the check here, we also verify that other
+        -- threads can access the dwarf info. But I'm not sure how to test that
+        -- a different unix thread can access the dwarf info as well.
+        dwarfIsLoaded >>= check           -- 3
+        dwarfTryUnload >>= (check . not)
+        dwarfIsLoaded >>= (check . not)
+        putMVar var2 ()
+    inDwarf $ do
+        dwarfIsLoaded >>= check           -- 2
+        dwarfTryUnload >>= (check . not)
+        putMVar var1 ()
+        takeMVar var2
+    inDwarf $ return ()
+    dwarfIsLoaded >>= check               -- 4
+    dwarfTryUnload >>= check
+
+
+check :: Bool -> IO ()
+check True  = putStrLn "OK"
+check False = putStrLn "Not OK"
+
